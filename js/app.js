82d1024232d98471599ee2ca1972745cd7a8e68e
Pooja <malvi.pooja259@gmail.com>
2017-08-09 16:56:50 +0530

    Navigation color change on scroll

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
-$(document).ready(function() {
-    var scroll_start = 0;
-    var startchange = $('#startchange');
-    var offset = startchange.offset();
-    if (startchange.length) {
-        $(document).scroll(function() {
-            scroll_start = $(this).scrollTop();
-            if (scroll_start > offset.top) {
-                $(".navbar").css('background-color', '#f0f0f0');
-            } else {
-                $('.navbar').css('background-color', 'transparent');
-            }
-        });
+var some = $('#scroll-start').offset().top;
+$(document).ready(function(){
+    $(window).on("scroll",function(){
+    var win = $(window).scrollTop();
+    if(win > some){
+        $(".navbar").css("background","rgba(255,255,255,1)");
     }
+    else{
+        $(".navbar").css("background","rgba(0,0,0,0)");
+    }
+  });
 });
